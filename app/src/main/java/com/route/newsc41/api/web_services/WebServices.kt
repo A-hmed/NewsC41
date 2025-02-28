c7c7b729c89627034fdc14c0868a83e6e280570a
Ahmed <ahmed.nabil1@nomadz.dev>
2025-02-28 11:01:19 +0200

    Use Coroutines.

--- app/src/main/java/com/route/newsc41/api/web_services/WebServices.kt
+++ app/src/main/java/com/route/newsc41/api/web_services/WebServices.kt
@@ -2,20 +2,19 @@ package com.route.newsc41.api.web_services
 
 import com.route.newsc41.api.model.ArticlesResponseDM
 import com.route.newsc41.api.model.SourcesResponseDM
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WebServices {
     @GET("/v2/everything")
-    fun getArticles(
+    suspend fun getArticles(
         @Query("apiKey") apiKey: String,
         @Query("sources") sourceId: String
-    ): Call<ArticlesResponseDM>
+    ): ArticlesResponseDM
 
     @GET("/v2/top-headlines/sources")
-    fun getSources(
+    suspend fun getSources(
         @Query("apiKey") apiKey: String,
         @Query("category") id: String
-    ): Call<SourcesResponseDM>
+    ): SourcesResponseDM
 }
--- app/src/main/java/com/route/newsc41/ui/home/fragments/news_fragment/NewsViewModel.kt
+++ app/src/main/java/com/route/newsc41/ui/home/fragments/news_fragment/NewsViewModel.kt
@@ -2,78 +2,45 @@ package com.route.newsc41.ui.home.fragments.news_fragment
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.gson.Gson
+import androidx.lifecycle.viewModelScope
 import com.route.newsc41.api.ApiManager
 import com.route.newsc41.api.model.ArticleDM
-import com.route.newsc41.api.model.ArticlesResponseDM
 import com.route.newsc41.api.model.SourceDM
-import com.route.newsc41.api.model.SourcesResponseDM
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NewsViewModel : ViewModel() {
     var sourcesApi = MutableLiveData<Resource<List<SourceDM?>>>()
     var articlesApi = MutableLiveData<Resource<List<ArticleDM>>>()
     fun getSources(categoryId: String) {
         sourcesApi.value = Resource.LoadingState()
-        ApiManager.getWebServices().getSources(ApiManager.apiKey, categoryId)
-            .enqueue(object : Callback<SourcesResponseDM> {
-                override fun onResponse(
-                    p0: Call<SourcesResponseDM>,
-                    response: Response<SourcesResponseDM>
-                ) {
-                    if (response.isSuccessful) {
-                        sourcesApi.value = Resource.SuccessState(response.body()!!.sources!!)
-                    } else {
-                        val errorResponse =
-                            Gson().fromJson(
-                                response.errorBody()!!.string(),
-                                SourcesResponseDM::class.java
-                            )
-                        sourcesApi.value =
-                            Resource.ErrorState(errorResponse.message ?: "Something went wrong")
-                    }
-
-                }
-
-                override fun onFailure(p0: Call<SourcesResponseDM>, p1: Throwable) {
-                    sourcesApi.value =
-                        Resource.ErrorState(p1.message ?: "Something went wrong")
-                }
-            })
-
-
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val sourcesResponseDM =
+                    ApiManager.getWebServices().getSources(ApiManager.apiKey, categoryId)
+                sourcesApi.value = Resource.SuccessState(sourcesResponseDM.sources!!)
+            } catch (e: Throwable) {
+                sourcesApi.value =
+                    Resource.ErrorState(e.message ?: "Something went wrong")
+            }
+        }
     }
 
     fun getArticles(sourceId: String) {
         articlesApi.value = Resource.LoadingState()
-        ApiManager.getWebServices().getArticles(ApiManager.apiKey, sourceId)
-            .enqueue(object : Callback<ArticlesResponseDM> {
-                override fun onResponse(
-                    p0: Call<ArticlesResponseDM>,
-                    response: Response<ArticlesResponseDM>
-                ) {
-                    if (response.isSuccessful) {
-                        articlesApi.value =
-                            Resource.SuccessState(response.body()?.articles ?: emptyList())
-                    } else {
-                        val errorResponse = Gson().fromJson(
-                            response.errorBody()?.string(),
-                            ArticlesResponseDM::class.java
-                        )
-                        articlesApi.value =
-                            Resource.ErrorState(errorResponse.message ?: "Something went wrong")
-                    }
-                }
+        viewModelScope.launch {
+            try {
+                val articlesResponse =
+                    ApiManager.getWebServices().getArticles(ApiManager.apiKey, sourceId)
+                articlesApi.value =
+                    Resource.SuccessState(articlesResponse.articles ?: emptyList())
+            } catch (e: Throwable) {
+                articlesApi.value =
+                    Resource.ErrorState(e.message ?: "Something went wrong")
 
-                override fun onFailure(p0: Call<ArticlesResponseDM>, ex: Throwable) {
-                    articlesApi.value =
-                        Resource.ErrorState(ex.message ?: "Something went wrong")
+            }
 
-                }
-
-            })
+        }
     }
 }
 
